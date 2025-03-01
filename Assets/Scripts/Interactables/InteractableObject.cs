f5c91b1c36ef4e2474d4e2b9f537cc07361817fd
MG2003 <max.guo2014@gmail.com>
2025-03-01 15:03:38 -0500

    delete stuff

--- Assets/Scripts/Interactables/InteractableObject.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface InteractableObject
-{
-    void Interact();
-}
--- Assets/Scripts/Interactables/NewMonoBehaviourScript.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class TestObject : MonoBehaviour, InteractableObject {
-    void InteractableObject.Interact(){
-        Debug.Log("This game should've made in godot");
-    }
-}
