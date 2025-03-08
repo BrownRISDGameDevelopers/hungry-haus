790e14374b9878f75dfa1b1a7e1cb06e1dc43035
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-03-08 15:06:24 -0500

    add ui toggle function

--- src/ui/puzzle.gd
+++ src/ui/puzzle.gd
@@ -2,20 +2,23 @@ extends Control
 
 class_name Puzzle
 
-# var is_visible : bool = false
-# var opaque : Color = Color(1,1,1,1)
-# var clear : Color = Color(1,1,1,0)
+var puzzle_active : bool = false
+var opaque : Color = Color(1,1,1,1)
+var clear : Color = Color(1,1,1,0)
 
-# func _ready():
-# 	modulate = Color(1,1,1,0)
+func _ready():
+	modulate = Color(1,1,1,0)
 
-# func _input(event):
-# 	if event.action_is_pressed("interact"):
-# 		print("toggling visibility")
-# 		ToggleVisibility()
+func _input(event):
+	if event.is_action_pressed("interact"):
+		ToggleVisibility()
+	else:
+		pass
 
-# func ToggleVisibility():
-# 	if is_visible:
-# 		modulate = clear
-# 	else:
-# 		modulate = opaque
+func ToggleVisibility():
+	if puzzle_active:
+		modulate = clear
+		puzzle_active = false
+	else:
+		modulate = opaque
+		puzzle_active = true
