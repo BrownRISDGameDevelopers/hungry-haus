0fd6ce50ce3daa9fca48d9cb196e870591d1b5b0
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-03-08 14:58:02 -0500

    setup main scene

--- src/ui/puzzle.gd
+++ src/ui/puzzle.gd
@@ -1,3 +1,21 @@
 extends Control
 
 class_name Puzzle
+
+# var is_visible : bool = false
+# var opaque : Color = Color(1,1,1,1)
+# var clear : Color = Color(1,1,1,0)
+
+# func _ready():
+# 	modulate = Color(1,1,1,0)
+
+# func _input(event):
+# 	if event.action_is_pressed("interact"):
+# 		print("toggling visibility")
+# 		ToggleVisibility()
+
+# func ToggleVisibility():
+# 	if is_visible:
+# 		modulate = clear
+# 	else:
+# 		modulate = opaque
