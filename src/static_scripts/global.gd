fa56399112f9d92b4de49d7ea288a3b1a29a780b
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-03-08 15:33:14 -0500

    add safe_tween static function

---
+++ src/static_scripts/global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+static var existing_tweens = {}
+
+static func safe_tween(node : Node) -> Tween:
+	if existing_tweens.has(node):
+		existing_tweens[node].kill()
+	var tween = node.create_tween()
+	existing_tweens[node] = tween
+	return tween
