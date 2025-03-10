34b933189d7de8afc9b33e1d2981bc07faa01b8d
bokai_bi <bokai.bi0@gmail.com>
2025-03-10 14:39:23 -0400

    feat: added global outline function

--- src/static_scripts/global.gd
+++ src/static_scripts/global.gd
@@ -8,3 +8,25 @@ static func safe_tween(node : Node) -> Tween:
 	var tween = node.create_tween()
 	existing_tweens[node] = tween
 	return tween
+
+static func show_3d_outline(meshNode: MeshInstance3D, color: Vector4, size: float):
+	var outlineObj: MeshInstance3D = meshNode.get_node_or_null("_outline")
+	if (!outlineObj):
+		outlineObj = MeshInstance3D.new()
+		meshNode.add_child(outlineObj)
+		outlineObj.name = "_outline"
+		#var surfaceCount: int = outlineObj.mesh.get_surface_count()
+		#for i in surfaceCount:
+			#outlineObj.mesh.surface_set_material(i, outlineMat)
+		var outlineMat: Material = preload("res://src/world/outline/outline.tres")
+		outlineObj.mesh = meshNode.mesh.duplicate(true)
+		outlineObj.mesh.material = outlineMat
+	outlineObj.mesh.material.set("shader_parameter/outline_color", color)
+	outlineObj.scale = Vector3(size, size, size)
+	outlineObj.visible = true
+
+static func hide_3d_outline(meshNode: MeshInstance3D):
+	var outlineObj: MeshInstance3D = meshNode.get_node_or_null("_outline")
+	if (!outlineObj):
+		print("Cannot find created outline object on meshNode " + meshNode.name)
+	outlineObj.visible = false
