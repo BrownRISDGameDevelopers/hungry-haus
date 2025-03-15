144862be81e285c7bc1dd16ea73b393a153970f2
henreedev <henry_earnest@brown.edu>
2025-03-15 16:33:55 -0400

    add timer, state

--- src/GlobalState.gd
+++ src/GlobalState.gd
@@ -1,6 +1,13 @@
 extends Node
 
+signal switched_room(to: Room.Type)
+
 var current_room : Room.Type = Room.Type.KITCHEN
 
 func move_to_next_room():
 	current_room = (int(current_room) + 1) as Room.Type
+	switched_room.emit(current_room)
+
+func lose():
+	print("LOST THE GAME!!!!!!!!")
+	# TODO trigger jumpscare
