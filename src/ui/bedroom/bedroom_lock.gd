ae7aeaaec80cd76425019ca37de260dfc26929d7
MG2003 <max.guo2014@gmail.com>
2025-03-15 16:33:42 -0400

    feedback and impl into world

--- src/ui/bedroom/bedroom_lock.gd
+++ src/ui/bedroom/bedroom_lock.gd
@@ -2,11 +2,26 @@ extends Puzzle
 
 class_name BedroomLock
 
+const TRANS_RED = Color(1, 0, 0, 0)
 func _ready():
-	$ColorRect/submit.pressed.connect(_submit)
-	modulate = Color(1,1,1,0)
+	$submit.pressed.connect(_submit)
+	modulate = Color(1,1,1,1)
 
 func _submit():
-	var combo = $ColorRect/HBoxContainer/LockButton.getVal() + $ColorRect/HBoxContainer/LockButton2.getVal() + $ColorRect/HBoxContainer/LockButton3.getVal() + $ColorRect/HBoxContainer/LockButton4.getVal() + $ColorRect/HBoxContainer/LockButton5.getVal()
+	var combo = $HBoxContainer/LockButton.getVal() + $HBoxContainer/LockButton2.getVal() + $HBoxContainer/LockButton3.getVal() + $HBoxContainer/LockButton4.getVal() + $HBoxContainer/LockButton5.getVal()
 	if combo == "12345":
 		print("Success!")
+		$status_overlay.modulate = Color(0, 1, 0)
+		Global.safe_tween($status_overlay).tween_property($status_overlay, "modulate", Color(0, 1, 0, 0) , APPEAR_DURATION_SEC)
+		$HBoxContainer/LockButton.disabled = true
+		$HBoxContainer/LockButton2.disabled = true
+		$HBoxContainer/LockButton3.disabled = true
+		$HBoxContainer/LockButton4.disabled = true
+		$HBoxContainer/LockButton5.disabled = true
+		$submit.disabled = true
+		complete()
+	else:
+		$status_overlay.modulate = Color(1, 0, 0)
+		
+		Global.safe_tween($status_overlay).tween_property($status_overlay, "modulate", TRANS_RED , APPEAR_DURATION_SEC)
+		
