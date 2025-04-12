a77f74d474023cedb83def2c10b9260514562e23
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-04-12 16:53:38 -0400

    make doors functional

--- src/world/door.gd
+++ src/world/door.gd
@@ -2,6 +2,14 @@ extends Node3D
 
 class_name Door
 
+@export var is_closet := false
+@export var door_animator : AnimationPlayer
+@export var closet_animator : AnimationPlayer
+@export var door_collider : CollisionShape3D
+
 func open():
-	create_tween().tween_property(self, "rotation", PI / 3, 0.3).set_trans(Tween.TRANS_BOUNCE)
+	door_animator.play("Open")
+	closet_animator.play("LeftAction")
+	door_collider.disabled = true
+	# create_tween().tween_property(self, "rotation", PI / 3, 0.3).set_trans(Tween.TRANS_BOUNCE)
 	# TODO play a door opening sound here
