b6f4c7e1379ea996355d7cb09f5461815996f9a3
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-04-19 16:29:08 -0400

    brighten object outline color

--- src/world/interactable_object.gd
+++ src/world/interactable_object.gd
@@ -5,7 +5,7 @@ var highlighted: bool
 ## The puzzle opened by this object on click.
 @onready var puzzle: Puzzle = get_tree().get_first_node_in_group("kitchen_puzzle")
 
-@export var highlightColor: Vector4 = Vector4(1, 1, 0, 1)
+@export var highlightColor: Vector4 = Vector4(1, 1, 1, 1)
 @export var highlightSize: float = 1.01
 
 func _ready() -> void:
