f931c037c1cdcd8cd65957c46060e65a88ea42bc
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-05-03 18:48:49 -0400

    hide lock on door open

--- src/world/door.gd
+++ src/world/door.gd
@@ -6,10 +6,12 @@ class_name Door
 @export var door_animator : AnimationPlayer
 @export var closet_animator : AnimationPlayer
 @export var door_collider : CollisionShape3D
+@export var lock : Node3D
 
 func open():
 	door_animator.play("Open")
 	closet_animator.play("LeftAction")
 	door_collider.disabled = true
+	if lock: lock.hide()
 	# create_tween().tween_property(self, "rotation", PI / 3, 0.3).set_trans(Tween.TRANS_BOUNCE)
 	# TODO play a door opening sound here
