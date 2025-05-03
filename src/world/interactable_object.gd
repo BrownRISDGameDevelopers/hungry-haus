3e24b230417658753ad28a92849d23e48250de72
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-05-03 17:18:33 -0400

    give interactable obejcts specific puzzlse

--- src/world/interactable_object.gd
+++ src/world/interactable_object.gd
@@ -4,28 +4,31 @@ class_name InteractableObject
 
 var highlighted: bool
 
+@export var puzzle_type : Puzzle.Type = Puzzle.Type.BEDROOM_PAINTING
 @export var mesh : MeshInstance3D
 
 ## The puzzle opened by this object on click.
-@onready var puzzle: Puzzle = get_tree().get_first_node_in_group("bedroom_puzzle")
-
-
-
+@onready var puzzle: Puzzle # = get_tree().get_first_node_in_group("kitchen_puzzle")
 
 ## Create exprot htat lets you set puzzle enum
 ## Setup puzzle enum in Global
 
-
-
 @export var highlightColor: Color = Color(1, 1, 0.2, 1)
 @export var highlightSize: float = 1.01
+@export var thresh_modifier := 1.0
 
 func _ready() -> void:
 	highlighted = false
 
+	for p in get_tree().get_nodes_in_group("puzzle"):
+		if p.puzzle_type == puzzle_type:
+			puzzle = p
+	if !puzzle:
+		print("Missing puzzle: ", puzzle_type)
+
 func _process(_delta: float) -> void:
 	# Update highlighting
-	var looked: bool = Player.player.is_looking_at(mesh)
+	var looked: bool = Player.player.is_looking_at(mesh, thresh_modifier)
 	if highlighted != looked:
 		if highlighted:
 			Global.hide_3d_outline(mesh)
@@ -34,10 +37,10 @@ func _process(_delta: float) -> void:
 		highlighted = looked
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
+	if event.is_action_pressed("interact") && visible && get_parent().visible && highlighted:
 		interact()
 
 ## Opens this interactable's puzzle if it's highlighted.
 func interact() -> void:
-	if puzzle and highlighted and not puzzle.puzzle_active:
+	if puzzle and not puzzle.puzzle_active:
 		puzzle.toggle_puzzle_active()
