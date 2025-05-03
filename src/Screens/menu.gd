3eeb1bc8f34c7de18cacbab4407a9fb498c24a23
henreedev <henry_earnest@brown.edu>
2025-05-03 17:19:01 -0400

    menu transition framework

--- src/Screens/menu.gd
+++ src/Screens/menu.gd
@@ -2,8 +2,15 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/Game.tscn")
+	play_intro_cutscene()
 
+
+func play_intro_cutscene():
+	# Disable all buttons
+	# Move camera towards doorway and
+	# Fade to black
+	# Slap ID card down
+	get_tree().change_scene_to_file("res://src/Game.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
